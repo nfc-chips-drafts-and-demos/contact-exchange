e93f882d8b22c0354147f7515426948ba5a9fbdb
tanzeelak <tnkhan8042@gmail.com>
2024-06-16 11:56:04 -0700

    redirect upon success

--- esm-nfcs/src/app/add-contact/page.tsx
+++ esm-nfcs/src/app/add-contact/page.tsx
@@ -2,11 +2,11 @@
 import { useCallback} from "react";
 // @ts-ignore
 import { execHaloCmdWeb } from "@arx-research/libhalo/api/web.js";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 
 export default async function AddContact() {
     const router = useRouter();
-    const linkTag = useCallback(async () => {
+    const linkContact = useCallback(async () => {
         let command = {
           name: "sign",
           keyNo: 1,
@@ -20,22 +20,27 @@ export default async function AddContact() {
           res = await execHaloCmdWeb(command);
           // the command has succeeded, display the result to the user
           // try to add them as a contact
-          await fetch("/api/add-contact", {
+          const addContactResult = await fetch("/api/add-contact", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({ publicKey: res.publicKey})
           })
+          const addContactData = await addContactResult.json()
+          if (addContactData.success) {
+            router.push("/me")
+          }
 
         } catch (e) {
           // the command has failed, display error to the user
           console.log(e);
+          router.push("/add-contact")
         }
       }, []);
     
 
   return (
     <div>
-        <button onClick={linkTag} >Scan NFC Tag</button>
+        <button className="bg-white border rounded border-gray-400 px-4 py-2 font-medium text-md" onClick={linkContact} >Scan Esmeraldan's NFC Tag</button>
     </div>
   );
 }
